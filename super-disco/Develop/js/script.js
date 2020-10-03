cf69b87ed40194511fa44998dc91b7ac4006ef10
Mona Lisa <joannagrosshans2@gmail.com>
2020-10-02 19:21:32 -0500

    clean up js

--- super-disco/Develop/js/script.js
+++ super-disco/Develop/js/script.js
@@ -1,18 +1,12 @@
 $(document).ready(function() {
 
 var currentDayEl = document.getElementById('currentDay');
-// var toDo = [];
 var input = [];
-// var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 var displayDay= moment().format("dddd MMMM do YYYY");
 console.log(displayDay);
 currentDayEl.textContent = displayDay;
-// $(".btn").on("click", function() {
-//     var value = $(this).siblings(".description").val()
-//     var time = $(this).parent().attr("id")
-//     localStorage.setItem(value, time);
-// });
+
 // sets inputs to array
 var input = JSON.parse(localStorage.getItem("input")) || [];
 
@@ -26,8 +20,6 @@ $(".hour1").on("click", function(event) {
 
     input.push(value1);
 
-    // $("#hour1").append(value1);
-
     localStorage.setItem("input", JSON.stringify(input));
 
 });
@@ -38,8 +30,6 @@ $(".hour2").on("click", function(event) {
         .trim();
     input.push(hour1)
 
-    // $("#hour2").append(hour2);
-
     localStorage.setItem("input", JSON.stringify(input));
 });
 $(".hour3").on("click", function(event) {
@@ -49,7 +39,6 @@ $(".hour3").on("click", function(event) {
         .trim();
     input.push(hour3)
 
-    // $("#hour3").append(hour3);
     localStorage.setItem("input", JSON.stringify(input));
 
 });
@@ -60,7 +49,6 @@ $(".hour4").on("click", function(event) {
         .trim();
     input.push(hour4)
 
-    // $("#hour4").append(hour4);
     localStorage.setItem("input", JSON.stringify(input));
 });
 $(".hour5").on("click", function(event) {
@@ -70,7 +58,6 @@ $(".hour5").on("click", function(event) {
         .trim();
     input.push(hour5)
 
-    // $("#hour5").append(hour5);
     localStorage.setItem("input", JSON.stringify(input));
 });
 $(".hour6").on("click", function(event) {
@@ -80,7 +67,6 @@ $(".hour6").on("click", function(event) {
         .trim();
     input.push(hour6)
 
-    //  $("#hour6").append(hour6);
      localStorage.setItem("input", JSON.stringify(input));
 });
 $(".hour7").on("click", function(event) {
@@ -90,7 +76,6 @@ $(".hour7").on("click", function(event) {
         .trim();
     input.push(hour7)
 
-    // $("#hour7").append(hour7);    
     localStorage.setItem("input", JSON.stringify(input));
 });
 $(".hour8").on("click", function(event) {
@@ -100,7 +85,6 @@ $(".hour8").on("click", function(event) {
         .trim();
     input.push(hour8)
 
-    // $("#hour8").append(hour8);
     localStorage.setItem("input", JSON.stringify(input));
 });
 $(".hour9").on("click", function(event) {
@@ -110,13 +94,11 @@ $(".hour9").on("click", function(event) {
         .trim();
     input.push(hour9)
 
-    // $("#hour9").append(hour9);
     localStorage.setItem("input", JSON.stringify(input));
     displayInput();
 });
 
 // set text to page
-// // todos on the page
 function displayInput() {
     
     // $(".content").empty();
@@ -124,13 +106,9 @@ function displayInput() {
     
     //iterates over the input list
     for (var i = 0; i < input.length; i++) {
-        // console.log("yipee")
         var toDoText = $("<span>")
             .addClass("task")
             .text();
-            console.log("soclose")
-        // toDoText.text(input[i]);
-        console.log("working")
 
     //adds input to the text
     $(".content").append(toDoText);
@@ -173,7 +151,6 @@ var nowTime = function() {
         $(this).addClass("future");
         // console.log("ok")
         }
-        // saveInput();
     });
 }
 
@@ -181,20 +158,9 @@ var nowTime = function() {
 displayInput();
 nowTime();
 });
-// $("#hour1 .content").val(localStorage.getItem("input1"));
-// console.log('input1')
-// $("#hour2 .content").val(localStorage.getItem("input2"));
-// $("#hour3 .content").val(localStorage.getItem("input3"));
-// $("#hour4 .content").val(localStorage.getItem("input4"));
-// $("#hour5 .content").val(localStorage.getItem("input5"));
-// $("#hour6 .content").val(localStorage.getItem("input6"));
-// $("#hour7 .content").val(localStorage.getItem("input7"));
-// $("#hour8 .content").val(localStorage.getItem("input8"));
-// $("#hour9 .content").val(localStorage.getItem("input9"));
 
-//saving storage through refresh
-//how to get buttons bigger/icon
-//hot to get text box bigger
+
+
 
 
 
