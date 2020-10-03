310fdaba4e611befc325ee259ee3fbda2251e5c7
Mona Lisa <joannagrosshans2@gmail.com>
2020-10-02 20:28:09 -0500

    button css changed

--- super-disco/Develop/js/script.js
+++ super-disco/Develop/js/script.js
@@ -1,45 +1,39 @@
-$(document).ready(function() {
-
 var currentDayEl = document.getElementById('currentDay');
-var input = [];
 
-var displayDay= moment().format("dddd MMMM do YYYY");
+var displayDay= moment().format("dddd MMMM Do YYYY");
 console.log(displayDay);
 currentDayEl.textContent = displayDay;
-
-// sets inputs to array
-var input = JSON.parse(localStorage.getItem("input")) || [];
 
 // //save entered comment
 $(".hour1").on("click", function(event) {
     event.preventDefault();
-    var value1= $("#hour1")
+    var hour1= $("#hour1")
         .val()
         .trim();
         console.log(value1)
 
-    input.push(value1);
+    // input.push(value1);
 
-    localStorage.setItem("input", JSON.stringify(input));
+    localStorage.setItem("time1", JSON.stringify(hour1));
 
 });
 $(".hour2").on("click", function(event) {
     event.preventDefault();
-    var hour1 = $("#hour2")
+    var hour2 = $("#hour2")
         .val()
         .trim();
-    input.push(hour1)
+    // input.push(hour1)
 
-    localStorage.setItem("input", JSON.stringify(input));
+    localStorage.setItem("time2", JSON.stringify(hour2));
 });
 $(".hour3").on("click", function(event) {
     event.preventDefault();
     var hour3 = $("#hour3")
         .val()
-        .trim();
-    input.push(hour3)
+        // .trim();
+    // input.push(hour3)
 
-    localStorage.setItem("input", JSON.stringify(input));
+    localStorage.setItem("time3", JSON.stringify(hour3));
 
 });
 $(".hour4").on("click", function(event) {
@@ -47,85 +41,94 @@ $(".hour4").on("click", function(event) {
     var hour4 = $("#hour4")
         .val()
         .trim();
-    input.push(hour4)
+    // input.push(hour4)
 
-    localStorage.setItem("input", JSON.stringify(input));
+    localStorage.setItem("time4", JSON.stringify(hour4));
 });
 $(".hour5").on("click", function(event) {
     event.preventDefault();
     var hour5 = $("#hour5")
         .val()
         .trim();
-    input.push(hour5)
+    // input.push(hour5)
 
-    localStorage.setItem("input", JSON.stringify(input));
+    localStorage.setItem("time5", JSON.stringify(hour5));
 });
 $(".hour6").on("click", function(event) {
     event.preventDefault();
     var hour6 = $("#hour6")
         .val()
         .trim();
-    input.push(hour6)
+    // input.push(hour6)
 
-     localStorage.setItem("input", JSON.stringify(input));
+     localStorage.setItem("time6", JSON.stringify(hour6));
 });
 $(".hour7").on("click", function(event) {
     event.preventDefault();
     var hour7 = $("#hour7")
         .val()
         .trim();
-    input.push(hour7)
+    // input.push(hour7)
 
-    localStorage.setItem("input", JSON.stringify(input));
+    localStorage.setItem("time7", JSON.stringify(hour7));
 });
 $(".hour8").on("click", function(event) {
     event.preventDefault();
     var hour8 = $("#hour8")
         .val()
         .trim();
-    input.push(hour8)
+    // input.push(hour8)
 
-    localStorage.setItem("input", JSON.stringify(input));
+    localStorage.setItem("time8", JSON.stringify(hour8));
 });
 $(".hour9").on("click", function(event) {
     event.preventDefault();
     var hour9 = $("#hour9")
         .val()
         .trim();
-    input.push(hour9)
+    // input.push(hour9)
 
-    localStorage.setItem("input", JSON.stringify(input));
+    localStorage.setItem("time9", JSON.stringify(hour9));
     displayInput();
 });
 
 // set text to page
 function displayInput() {
-    
+    // sets diret input to textarea
+var time1 = JSON.parse(localStorage.getItem("time1"));
+var time2 = JSON.parse(localStorage.getItem("time2"));
+var time3 = JSON.parse(localStorage.getItem("time3"));
+var time4 = JSON.parse(localStorage.getItem("time4"));
+var time5 = JSON.parse(localStorage.getItem("time5"));
+var time6 = JSON.parse(localStorage.getItem("time6"));
+var time7 = JSON.parse(localStorage.getItem("time7"));
+var time8 = JSON.parse(localStorage.getItem("time8"));
+var time9 = JSON.parse(localStorage.getItem("time9"));
+
     // $(".content").empty();
-     console.log("noway")
-    
-    //iterates over the input list
-    for (var i = 0; i < input.length; i++) {
+    // console.log("noway")
+        //iterates over the input list
+    // for (var i = 0; i < input.length; i++) {
         var toDoText = $("<span>")
-            .addClass("task")
-            .text();
+        .addClass("task")
+        .text();
 
     //adds input to the text
     $(".content").append(toDoText);
-    
-        // replace textarea with p element
-        $(".block-0").replaceWith(input[0]);
-        $(".block-1").replaceWith(input[1]);
-        $(".block-2").replaceWith(input[2]);
-        $(".block-3").replaceWith(input[3]);
-        $(".block-4").replaceWith(input[4]);
-        $(".block-5").replaceWith(input[5]);
-        $(".block-6").replaceWith(input[6]);
-        $(".block-7").replaceWith(input[7]);
-        $(".block-8").replaceWith(input[8]);
-        console.log("madeit")
+        
+    // replace textarea with input
+    $(".block-0").replaceWith(time1);
+    $(".block-1").replaceWith(time2);
+    $(".block-2").replaceWith(time3);
+    $(".block-3").replaceWith(time4);
+    $(".block-4").replaceWith(time5);
+    $(".block-5").replaceWith(time6);
+    $(".block-6").replaceWith(time7);
+    $(".block-7").replaceWith(time8);
+    $(".block-8").replaceWith(time9);
+    console.log("madeit")
     }
-};
+// };
 //set current time to compare to scheduled time
 var compareTime = moment().hours();  // give us the current hour in military format 
 console.log(compareTime);
@@ -152,12 +155,13 @@ var nowTime = function() {
         // console.log("ok")
         }
     });
-}
+
+};
 
 // when page loads
 displayInput();
 nowTime();
-});
+
 
 
 
